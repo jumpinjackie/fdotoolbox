97ccfc550db23909f4e196baf923ebf40e6b664e
Jackie Ng <jumpinjackie@gmail.com>
2020-04-30 17:30:39 +1000

    Bring in FdoTest runner from coordsys sandbox

---
+++ FdoTest/Assert.cs
@@ -0,0 +1,127 @@
+﻿#region LGPL Header
+// Copyright (C) 2020, Jackie Ng
+// https://github.com/jumpinjackie/fdotoolbox, jumpinjackie@gmail.com
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+//
+// See license.txt for more/additional licensing information
+#endregion
+
+using FdoToolbox.Core.Feature;
+using OSGeo.FDO.Schema;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace FdoTest
+{
+    // Why are we inventing our own test framework and not using a battle-tested 
+    // unit test framework like nUnit or xUnit?
+    //
+    // Because our code under test has bindings to unmanaged code and rather than
+    // wasting time/resources to figure out how to get such code to properly run in
+    // their respective runners without errors around missing dlls, we could just
+    // re-create the core primitives of any test framework (Assert, etc) and just
+    // roll our own test runner with the right unmanaged dll setup
+
+    [Serializable]
+    public class AssertException : Exception
+    {
+        public AssertException() { }
+        public AssertException(string message, string caller)
+            : base(message) 
+        {
+            this.Caller = caller;
+        }
+        public AssertException(string message, Exception inner) : base(message, inner) { }
+
+        public string Caller { get; }
+        protected AssertException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+
+    public static class Assert
+    {
+        static string FormatCaller(string member, string sourcePath, int lineNum)
+            => $"at {member} ({sourcePath}, line: {lineNum})";
+
+        public static void Throws<TException>(
+            Action action,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0) where TException : Exception
+        {
+            try
+            {
+                action.Invoke();
+                Assert.Fail($"Expected exception of type {typeof(TException)} to be thrown", memberName, sourceFilePath, sourceLineNumber);
+            }
+            catch (TException)
+            {
+
+            }
+        }
+
+        public static void Fail(
+            string failMessage,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0) 
+            => throw new AssertException(failMessage, FormatCaller(memberName, sourceFilePath, sourceLineNumber));
+
+        internal static void NotNull(
+            object obj,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        {
+            if (obj == null)
+                throw new AssertException("Expected given object to not be null", FormatCaller(memberName, sourceFilePath, sourceLineNumber));
+        }
+
+        public static void Equal<T>(
+            T expected,
+            T value,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+            => Equal(expected, value, $"Expected: {expected}, Got: {value}", memberName, sourceFilePath, sourceLineNumber);
+
+        public static void Equal<T>(
+            T expected,
+            T value,
+            string failMessage,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        {
+            if (!EqualityComparer<T>.Default.Equals(expected, value))
+                throw new AssertException(failMessage, FormatCaller(memberName, sourceFilePath, sourceLineNumber));
+        }
+
+        internal static void NotEmpty<T>(
+            IEnumerable<T> sourceScs,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        {
+            if (!sourceScs.Any())
+                throw new AssertException("Expected a non-empty sequence/collection", FormatCaller(memberName, sourceFilePath, sourceLineNumber));
+        }
+    }
+}
---
+++ FdoTest/Program.cs
@@ -0,0 +1,124 @@
+﻿#region LGPL Header
+// Copyright (C) 2020, Jackie Ng
+// https://github.com/jumpinjackie/fdotoolbox, jumpinjackie@gmail.com
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+//
+// See license.txt for more/additional licensing information
+#endregion
+
+using FdoToolbox.Core;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace FdoTest
+{
+    class Program
+    {
+        static int _testsRun;
+        static int _failedTests;
+        static int _erroredTests;
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine("FDO Toolbox test runner");
+
+            string dir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string path = Path.Combine(dir, "FDO");
+            FdoAssemblyResolver.InitializeFdo(path);
+
+            InvokeTest(Test_TestFrameworkDogfood);
+
+            Console.WriteLine("===============================");
+            Console.WriteLine("Test Summary:");
+            Console.WriteLine($"{_testsRun} run, {_failedTests} failed, {_erroredTests} errored");
+
+            System.GC.Collect();
+
+            return _failedTests + _erroredTests;
+        }
+
+        static void InvokeTest(Action suite)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            try
+            {
+                _testsRun++;
+                Console.WriteLine($">> Executing test: {suite.Method.Name}");
+                suite.Invoke();
+                sw.Stop();
+                Console.WriteLine($">>    Test Result: OK ({sw.ElapsedMilliseconds} ms)");
+            }
+            catch (AssertException ex)
+            {
+                sw.Stop();
+                _failedTests++;
+                Console.WriteLine($">>    Test Result: Failed ({sw.ElapsedMilliseconds} ms)");
+                Console.WriteLine($">>    Message was:");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine($"  {ex.Caller}");
+            }
+            catch
+            {
+                sw.Stop();
+                _erroredTests++;
+                Console.WriteLine($">>    Test Result: Errored ({sw.ElapsedMilliseconds} ms)");
+            }
+            finally
+            {
+                //HACK: If we don't do this we may hit an access violation when the GC claims a
+                //FDO .net class proxy
+                System.GC.Collect();
+            }
+        }
+
+        static void Test_TestFrameworkDogfood()
+        {
+            Assert.Equal(1, 1);
+            Assert.Equal(1, 1.0);
+            Assert.Equal(1.0, 1);
+            Assert.Equal(1, 1.0f);
+            Assert.Equal(1.0f, 1);
+            try
+            {
+                Assert.Equal(1, 2);
+            }
+            catch (AssertException ex)
+            {
+                Assert.Equal($"Expected: {1}, Got: {2}", ex.Message);
+            }
+            Assert.Throws<AssertException>(() => Assert.Equal(1, 2));
+            try
+            {
+                Assert.Throws<NotSupportedException>(() => Assert.Equal(1, 1));
+            }
+            catch (AssertException ex)
+            {
+                Assert.Equal($"Expected exception of type {typeof(NotSupportedException)} to be thrown", ex.Message);
+            }
+            try
+            {
+                Assert.Fail("This is not supposed to happen");
+            }
+            catch (AssertException ex)
+            {
+                Assert.Equal("This is not supposed to happen", ex.Message);
+            }
+        }
+    }
+}
